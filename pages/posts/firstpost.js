b6fd60d1d2344ce5e556e8ed93283ce8ed1d5a40
Om Sharma <omsharma.theplayer11@gmail.com>
2022-05-07 23:23:12 +0530

    styling in nextjs

--- pages/posts/firstpost.js
+++ pages/posts/firstpost.js
@@ -1,8 +1,20 @@
 import Link from "next/link";
-
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
 export default function firstpost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title> heading</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1> This is my first code in Next.js</h1>
       <h2>
         {" "}
@@ -10,6 +22,6 @@ export default function firstpost() {
           <a> Back to home </a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
